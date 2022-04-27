f07dccb54f5eedd0e4e302003d009ab578b0a2ec
Lucas Mofardini <lucasmofardini@gmail.com>
2022-04-26 23:43:42 -0300

    Nav bar e background svg

--- store-list/src/app/app.module.ts
+++ store-list/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { ListComponent } from './shared/list/list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule
