3d158b51b86d5b5d49a2ad7663c54828146f7bf1
Lucas Mofardini <lucasmofardini@gmail.com>
2022-04-30 19:10:24 -0300

    [StoreList] Populei 9 itens

--- Projetos/store-list/src/app/app.component.ts
+++ Projetos/store-list/src/app/app.component.ts
@@ -3,8 +3,81 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'store-list';
+  list = [
+    {
+      nome: 'Computador Desktop',
+      url: 'https://www.saldaodainformatica.com.br/3933-home_default/computador-desktop-lenovo-g71-sff-intel-core-i3-2120-4gb-hd-500gb-preto-microsoft-windows-7-professional.jpg',
+      preco: '4.899,00',
+      vezes_parceladas: 10,
+      preco_parcela: '500',
+      juros: 'sem juros',
+    },
+    {
+      nome: 'Iphone 13s',
+      url: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1645572386470',
+      preco: '8.899,00',
+      preco_parcela: '1.800',
+      vezes_parceladas: 5,
+      juros: '1% de juros',
+    },
+    {
+      nome: 'Samsung s20 fe',
+      url: 'https://files.tecnoblog.net/wp-content/uploads/2021/01/galaxy-s20-fe-produtos-2-700x700.png',
+      preco: '1.899,00',
+      preco_parcela: '200',
+      vezes_parceladas: 10,
+      juros: 'sem juros',
+    },
+    {
+      nome: 'Samsung Galaxy A32',
+      url: 'https://alliedecommerce.s3.amazonaws.com/desbloqueados_production/product_images/0001/6866/large.png?1630092028',
+      preco: '1.499',
+      vezes_parceladas: 10,
+      preco_parcela: '149,00',
+      juros: 'sem juros',
+    },
+    {
+      nome: 'Iphone 13s',
+      url: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1645572386470',
+      preco: '8.899,00',
+      preco_parcela: '1.800',
+      vezes_parceladas: 5,
+      juros: '1% de juros',
+    },
+    {
+      nome: 'Samsung s20 fe',
+      url: 'https://files.tecnoblog.net/wp-content/uploads/2021/01/galaxy-s20-fe-produtos-2-700x700.png',
+      preco: '1.899,00',
+      preco_parcela: '200',
+      vezes_parceladas: 10,
+      juros: 'sem juros',
+    },
+    {
+      nome: 'Computador Desktop',
+      url: 'https://www.saldaodainformatica.com.br/3933-home_default/computador-desktop-lenovo-g71-sff-intel-core-i3-2120-4gb-hd-500gb-preto-microsoft-windows-7-professional.jpg',
+      preco: '4.899,00',
+      vezes_parceladas: 10,
+      preco_parcela: '500',
+      juros: 'sem juros',
+    },
+    {
+      nome: 'Iphone 13s',
+      url: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1645572386470',
+      preco: '8.899,00',
+      preco_parcela: '1.800',
+      vezes_parceladas: 5,
+      juros: '1% de juros',
+    },
+    {
+      nome: 'Samsung s20 fe',
+      url: 'https://files.tecnoblog.net/wp-content/uploads/2021/01/galaxy-s20-fe-produtos-2-700x700.png',
+      preco: '1.899,00',
+      preco_parcela: '200',
+      vezes_parceladas: 10,
+      juros: 'sem juros',
+    },
+  ];
 }
