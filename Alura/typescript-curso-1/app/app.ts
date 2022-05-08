ac282bb095beedfc08ae0de81dcd8bf5fa28e280
Lucas Mofardini <lucasmofardini@gmail.com>
2022-05-08 15:48:57 -0300

    [Alura] TypeScript 2

---
+++ Alura/typescript-curso-1/app/app.ts
@@ -0,0 +1,4 @@
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 200,1);
+console.log(negociacao);
--- Alura/typescript-curso-1/dist/js/app.js
+++ Alura/typescript-curso-1/dist/js/app.js
@@ -1,6 +1,3 @@
 import { Negociacao } from "./models/negociacao.js";
-
-const negociacao = new Negociacao(new Date(), 1, 200);
-
-
+const negociacao = new Negociacao(new Date(), 200, 1);
 console.log(negociacao);
