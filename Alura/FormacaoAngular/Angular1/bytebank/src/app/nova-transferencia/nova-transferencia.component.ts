56eda1cc437562e9bcb57de44d1a51c026ec7874
Lucas Mofardini <lucasmofardini@gmail.com>
2022-06-15 23:54:56 -0300

    [Alura] Angular 1 - Modulo 7 FINISHED

--- Alura/FormacaoAngular/Angular1/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ Alura/FormacaoAngular/Angular1/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
 import { Transferencia } from "../services/models/transferencia.models";
 import { TransferenciaService } from "../services/transferencia.service";
 
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent{
     valor: number;
     destino:number;
 
-    constructor(private service: TransferenciaService){}
+    constructor(private service: TransferenciaService, private router: Router){}
 
     transferir(){
         const valorEmitir: Transferencia = {
@@ -25,8 +26,8 @@ export class NovaTransferenciaComponent{
         this.service
         .adicionar(valorEmitir)
         .subscribe(resultado => {
-            console.log(resultado);
             this.limparCampos();
+            this.router.navigateByUrl('extrato');
         }, 
         (error)=>{
             console.error(error);
